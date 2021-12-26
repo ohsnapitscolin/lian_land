91893408b69e15ed2209ab80d4d65bbc51afe353
Colin Dunn <colin@ritual.com>
2021-12-26 10:53:23 -0500

    Fix: Video Autoplay with Placeholder

--- src/components/slides/video.js
+++ src/components/slides/video.js
@@ -14,27 +14,35 @@ const Video = styled.video`
   object-fit: cover;
 `;
 
+const Placeholder = styled.div`
+  background-color: green;
+`;
+
 export default function VideoSlide({ video }) {
   const videoRef = useRef(null);
   const { ref, inView } = useInView({
-    threshold: 0
+    threshold: 0,
   });
 
   const file = video.source.file;
 
-  if (videoRef.current) {
-    if (inView && videoRef.current) {
-      videoRef.current.play();
-    } else {
-      videoRef.current.pause();
-    }
-  }
+  // if (videoRef.current) {
+  //   if (inView && videoRef.current) {
+  //     videoRef.current.play();
+  //   } else {
+  //     videoRef.current.pause();
+  //   }
+  // }
 
   return (
     <VideoWrapper ref={ref}>
-      <Video loop={true} muted ref={videoRef}>
-        <source src={file.url} type={file.contentType} />
-      </Video>
+      {inView ? (
+        <Video loop={true} muted ref={videoRef} playsInline autoPlay>
+          <source src={file.url} type={file.contentType} />
+        </Video>
+      ) : (
+        <Placeholder />
+      )}
     </VideoWrapper>
   );
 }
